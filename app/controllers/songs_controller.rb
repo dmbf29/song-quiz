777e278071dde2e557c787f310a5907e2f39bfbe
Douglas Berkley <douglasmberkley@gmail.com>
2022-11-11 18:04:18 +0900

    quiz correction

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,45 @@
 class SongsController < ApplicationController
   # /search?query=thriller
+  # params[:query]
   def search
+    # @songs = Song.where(title: params[:query])
+    @songs = Song.where('title ILIKE ?', "%#{params[:query]}%")
   end
 
   # /songs/named/thriller
+  # params[:name]
   def search_two
+    # @songs = Song.where(title: params[:name])
+    @songs = Song.where('title ILIKE ?', "%#{params[:name]}%")
+  end
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def song_params
+    # Why do we have to filter parameters using “strong params” in the controller?
+    # To keep the bad guys out of our DB
+    # Restrict user from giving us params we don't want
+    params.require(:song).permit(:title, :year, :category)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
-  get 'songs', to: 'songs#index'
-  post 'songs', to: 'songs#create'
-
   get '/search', to: 'songs#search'
   get '/songs/named/:name', to: 'songs#search_two'
+
+  # Read all
+  get '/songs', to: 'songs#index', as: :songs
+  # Create
+  get '/songs/new', to: 'songs#new', as: :new_song
+  post '/songs', to: 'songs#create'
+  # Read one
+  get '/songs/:id', to: 'songs#show', as: :song
+  # Update
+  get '/songs/:id/edit', to: 'songs#edit', as: :edit_song
+  patch '/songs/:id', to: 'songs#update'
+  # Destroy
+  delete '/songs/:id', to: 'songs#destroy'
+
+  resources :songs do
+    resources :reviews, only: [:new, :create]
+  end
 end
